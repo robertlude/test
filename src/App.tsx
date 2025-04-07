86dcebf8de3d2cda22287eb3772cf0f83f6ccfbe
Robert Lude <robert.lude@gmail.com>
2025-04-07 14:58:22 -0400

    experiment #1 grab token

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 import React, { useEffect /*, useState */ } from 'react';
 import './App.css';
 
+async function getData(code: string) {
+  const token = await fetch(
+    'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token',
+    {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': 'https://robertlude.github.io'
+      },
+      body: JSON.stringify({
+        client_id: 'Iv23ctSG8C0iaSaB8a6Y',
+        code: code,
+      }),
+    }
+  ).then(response => response.json())
+  
+  console.log({token})
+
+
+  // const response = await fetch('https://api.github.com/repos/robertlude/test/contents/package.json', {
+  //   method: 'GET',
+  //   headers: {
+  //     'Authorization': `Bearer ${token}`,
+  //   },
+  // })
+
+  // return response.json()
+
+  return {}
+}
+
 function App() {
   const code = new URLSearchParams(window.location.search).get('code')
   //const [contents, setContents] = useState<string>(null)
@@ -8,14 +38,7 @@ function App() {
   useEffect(() => {
     if (!code) return
 
-    fetch('https://api.github.com/repos/robertlude/test/contents/package.json', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${code}`,
-      },
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
+    getData(code).then(data => console.log(data))
   }, [code])
   
   return (
