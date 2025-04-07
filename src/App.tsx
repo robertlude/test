444144106d28fe83a1555d3dd8f3bc1f40123b5e
Robert Lude <robert.lude@gmail.com>
2025-04-07 14:40:55 -0400

    add login with github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,20 +19,7 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://github.com/login/oauth/authorize?client_id=Iv23ctSG8C0iaSaB8a6Y">Login with GitHub</a>
     </div>
   );
 }
